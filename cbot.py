86c5a4e30d2078ffeb8894ef1697c8926830f498
theworm4002 <62223975+theworm4002@users.noreply.github.com>
2021-10-20 19:26:09 -0400

    Update cbot.py
    added send def
    added main def 
    added DebugMode var
    added printing to screen the outgoing msg as well 
    fixed indents

--- cbot.py
+++ cbot.py
@@ -1,33 +1,56 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+#
+#
 
+import sys
+import time
+import socket
+import string
 from config import *
 
-import socket
-import sys
-import string
-import time
+DebugMode = True
 
 sockChan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sockChan.connect_ex((BotServer, BotPort))
 
-sockChan.connect((BSERVER, BPORT))
-sockChan.send('NICK ' + BNICK + '\r\n'.encode('UTF-8'))
-sockChan.send('USER ' + BREALNAME +  ' * * :' + BIDENT + '\r\n'.encode('UTF-8'))
 
-while 1: 
-    line = sockChan.recv(2040)
-    print(line)
-    lline = line.split()
-    if 'PING' in lline[0]:
-        sockChan.send("PONG " + lline[1] + "\r\n")
-    if '001' in lline[1]:
-        sockChan.send("JOIN " + BCHANNEL + "\r\n")
-       # sockChan.send("PRIVMSG NICKSERV :IDENTIFY " + BPASSWORD + "\r\n")
-    if '433' in lline[1]:
-        sockChan.send("NICK " + BALT + "\r\n")
-    if 'PRIVMSG' in lline[1]:
-      if '!quit' in lline[3]:
-         sockChan.send("QUIT \r\n")
-      if '!join' in lline[3]:
-        sockChan.send("JOIN " + lline[4] + "\r\n")
-      if '!part' in lline[3]:
-        sockChan.send("PART " + lline[4] + "\r\n")
+def sendIRC(msg):
+    sockChan.sendall(bytes(f'{str(msg)} \r\n', 'UTF-8')) # Send data to IRC server.
+    if DebugMode:
+        print(f'Sending to Server: {msg}')
+
+
+sendIRC(f'USER {BotIdent} * * :{BotRealName}') #Command: USER Parameters: <user> <mode> <unused> <realname>
+sendIRC('NICK ' + BotNick)
+if BotPassword:
+    sendIRC('PRIVMSG NICKSERV :IDENTIFY ' + BotPassword)
+
+connected = True
+
+def main():
+    while connected:
+
+        line = sockChan.recv(2040).decode('utf-8')
+
+        if DebugMode:
+            print(line)
+    
+        lline = line.split()
+
+        if 'PING' in lline[0]:
+            sendIRC("PONG " + lline[1] )
+        if '001' in lline[1]:
+            sockChan.send("JOIN " + BotChannel )
+        if '433' in lline[1]:
+            sendIRC('NICK ' + BotAlt )
+
+        if 'PRIVMSG' in lline[1]:
+            if '!quit' in lline[3]:
+                sendIRC('QUIT')
+            if '!join' in lline[3]:
+                sendIRC('JOIN ' + lline[4] )
+            if '!part' in lline[3]:
+                sendIRC('PART ' + lline[4] )
+
+
+main()
